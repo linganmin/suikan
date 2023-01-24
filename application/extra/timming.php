dfe1a52f7c6e004bd646f5dd29737b4919c38362
小下 <saboran@163.com>
2023-01-24 22:48:42 +0800

    server

--- application/extra/timming.php
+++ application/extra/timming.php
@@ -1,27 +1,75 @@
 <?php
 return array (
-  'aa' => 
-  array (
-    'id' => 'aa',
-    'status' => '0',
-    'name' => 'aa',
-    'des' => '采集今日数据',
-    'file' => 'collect',
-    'param' => 'ac=cjday&xt=1&ct=&rday=24&cjflag=tv6_com&cjurl=http://cj2.tv6.com/mox/inc/youku.php',
-    'weeks' => '1,2,3,4,5,6,0',
-    'hours' => '00,01,02,03,04,05,06,07,08,09,10,11,12,13,14,15,16,17,18,19,20,21,22,23',
-    'runtime' => 1597629775,
-  ),
   'bb' => 
   array (
-    'status' => '0',
+    'id' => 'bb',
+    '__token__' => '6e942c785a2effc3fb3afd9d59d7e686',
+    'status' => '1',
     'name' => 'bb',
     'des' => '生成首页',
     'file' => 'make',
     'param' => 'ac=index',
     'weeks' => '1,2,3,4,5,6,0',
     'hours' => '00,01,02,03,04,05,06,07,08,09,10,11,12,13,14,15,16,17,18,19,20,21,22,23',
-    'id' => 'bb',
-    'runtime' => 1535348998,
+  ),
+  'gen_static_type' => 
+  array (
+    '__token__' => 'c814e4ba4eec60d2afe405de12afad59',
+    'status' => '1',
+    'name' => 'gen_static_type',
+    'des' => '生成视频分类',
+    'file' => 'make',
+    'param' => 'ac=type&tab=vod&vodtype=20,21,22,23,24',
+    'weeks' => '1,2,3,4,5,6,0',
+    'hours' => '00,01,02,03,04,05,06,07,08,09,10,11,12,13,14,15,16,17,18,19,20,21,22,23',
+    'runtime' => 1674566806,
+  ),
+  'gen_static_detail' => 
+  array (
+    'id' => 'gen_static_detail',
+    '__token__' => 'addba71d662b14f7bda8738f2b96c18b',
+    'status' => '1',
+    'name' => 'gen_static_detail',
+    'des' => '生成视频详情',
+    'file' => 'make',
+    'param' => 'ac=info&tab=vod&ac2=nomake',
+    'weeks' => '1,2,3,4,5,6,0',
+    'hours' => '00,01,02,03,04,05,06,07,08,09,10,11,12,13,14,15,16,17,18,19,20,21,22,23',
+    'runtime' => 1674569219,
+  ),
+  'collect_8' => 
+  array (
+    'id' => 'collect_8',
+    '__token__' => '5e75533c03abd037d3761d6619e9821f',
+    'status' => '1',
+    'name' => 'collect_8',
+    'des' => '当日采集8',
+    'file' => 'collect',
+    'param' => 'ac=cj&cjflag=992ae5cbb1c5c2b16e7d2858d94a9384&cjurl=https%3A%2F%2Fapi.1080zyku.com%2Finc%2Fapi_mac10.php&h=24&t=&ids=&wd=&type=2&mid=1&opt=0&sync_pic_opt=0&filter=0&filter_from=&param=',
+    'weeks' => '1,2,3,4,5,6,0',
+    'hours' => '03,09,16,20,21',
+  ),
+  'collect_7' => 
+  array (
+    'id' => 'collect_7',
+    '__token__' => '267f981f639768358ab6e32d2fbd6765',
+    'status' => '1',
+    'name' => 'collect_7',
+    'des' => '当日采集7',
+    'file' => 'collect',
+    'param' => 'ac=cj&cjflag=6c4b9deb0102572741f5169070ce1b56&cjurl=https%3A%2F%2Fapi.apibdzy.com%2Fapi.php%2Fprovide%2Fvod%2F%3Fac%3Dlist&h=24&t=&ids=&wd=&type=2&mid=1&opt=0&sync_pic_opt=0&filter=0&filter_from=&param=',
+    'weeks' => '1,2,3,4,5,6,0',
+    'hours' => '02,07,12,21',
+  ),
+  'collect_4' => 
+  array (
+    '__token__' => '71389ecfc447f6acb25be932df4439df',
+    'status' => '1',
+    'name' => 'collect_4',
+    'des' => '当日采集4',
+    'file' => 'collect',
+    'param' => 'ac=cj&cjflag=fceef6250232feb7d277b162be0ddc2c&cjurl=https%3A%2F%2Fapi.tiankongapi.com%2Fapi.php%2Fprovide%2Fvod%2Ffrom%2Ftkm3u8%2Fat%2Fxml%2F&h=24&t=&ids=&wd=&type=1&mid=1&opt=0&sync_pic_opt=1&filter=0&filter_from=&param=',
+    'weeks' => '1,2,3,4,5,6,0',
+    'hours' => '02,06,14,20,21,23',
   ),
 );
