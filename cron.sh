72dc7aa14cc8070b2cf8431c53abc9d94a31464b
linganmin <saboran@163.com>
2023-02-07 07:51:13 +0800

    cron

--- cron.sh
+++ cron.sh
@@ -1,42 +1,42 @@
 #!/usr/bin/env sh
 
-curl --request GET -sL --url 'https://suikan.linganmin.cn/api.php/timming/index.html?name=collect_8'
+curl --request GET -sL --url 'https://suikan.onetools.online/api.php/timming/index.html?name=collect_8'
 
 echo "\r\n"
 
-curl --request GET -sL --url 'https://suikan.linganmin.cn/api.php/timming/index.html?name=collect_7'
+curl --request GET -sL --url 'https://suikan.onetools.online/api.php/timming/index.html?name=collect_7'
 
 echo "\r\n"
 
-curl --request GET -sL --url 'https://suikan.linganmin.cn/api.php/timming/index.html?name=collect_4'
+curl --request GET -sL --url 'https://suikan.onetools.online/api.php/timming/index.html?name=collect_4'
 
 echo "\r\n"
 
-curl --request GET -sL --url 'https://suikan.linganmin.cn/api.php/timming/index.html?name=collect_9'
+curl --request GET -sL --url 'https://suikan.onetools.online/api.php/timming/index.html?name=collect_9'
 
 echo "\r\n"
 
-curl --request GET -sL --url 'https://suikan.linganmin.cn/api.php/timming/index.html?name=collect_10'
+curl --request GET -sL --url 'https://suikan.onetools.online/api.php/timming/index.html?name=collect_10'
 
 echo "\r\n"
 
-curl --request GET -sL --url 'https://suikan.linganmin.cn/api.php/timming/index.html?name=collect_11'
+curl --request GET -sL --url 'https://suikan.onetools.online/api.php/timming/index.html?name=collect_11'
 
 echo "\r\n"
 
-curl --request GET -sL --url 'https://suikan.linganmin.cn/api.php/timming/index.html?name=collect_12'
+curl --request GET -sL --url 'https://suikan.onetools.online/api.php/timming/index.html?name=collect_12'
 
 echo "\r\n"
 
 
 
-curl --request GET -sL --url 'https://suikan.linganmin.cn/api.php/timming/index.html?enforce=1&name=gen_static_detail'
+curl --request GET -sL --url 'https://suikan.onetools.online/api.php/timming/index.html?enforce=1&name=gen_static_detail'
 echo "\r\n"
 
-curl --request GET -sL --url 'https://suikan.linganmin.cn/api.php/timming/index.html?enforce=1&name=bb'
+curl --request GET -sL --url 'https://suikan.onetools.online/api.php/timming/index.html?enforce=1&name=bb'
 
 echo "\r\n"
 
-curl --request GET -sL --url 'https://suikan.linganmin.cn/api.php/timming/index.html?enforce=1&name=gen_static_type'
+curl --request GET -sL --url 'https://suikan.onetools.online/api.php/timming/index.html?enforce=1&name=gen_static_type'
 
 echo "\r\n"
